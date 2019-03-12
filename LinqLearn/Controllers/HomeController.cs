80a25fc92b51d7186591f8709a9f597f55b9e5d4
anjoe <775083111@qq.com>
2019-03-12 16:57:44 +0800

    base

---
+++ LinqLearn/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Linq;
+using System.Linq;
+using System.Web.Mvc;
+using LinqLearn.DataBase;
+
+namespace LinqLearn.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDataContext ctx = new MyDataContext();
+
+        public ActionResult Index()
+        {
+            ViewData["TableTest"] =Select();
+            Insert();
+            Update();
+            Delete();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public string Select()
+        {
+            string str_a = "";
+            var query1 = from p in ctx.UserLoginLogs
+                             //  where q.UserName == "789" //条件查询
+                                where p.UserName.Contains("78")    //模糊查询
+                                where p.TimeStamp.Contains("98") && p.IP == "11.11.11.11"    //多条件查询 
+                         select p;
+            foreach (var _user in query1)
+            {
+                str_a += _user.ID.ToString();
+            }
+            return str_a;
+        }
+        private void Insert() {
+            //插入数据
+            UserLoginLog ull = new UserLoginLog {
+                User_ID = "a100",
+                UserName = "Kim",
+                IP = "20190312",
+                TimeStamp =Guid.NewGuid().ToString("N").Substring(0,12)
+            };
+            ctx.UserLoginLogs.InsertOnSubmit(ull);
+            ctx.SubmitChanges();
+        }
+
+        private void Update() {
+            //更新数据
+            var query1 = (from q in ctx.UserLoginLogs
+                          where q.UserName == "789"
+                          select q).SingleOrDefault();
+            //判断query1是否为空，若不为空，则修改IP。
+            if (query1 != null)
+            {
+                query1.IP = "123.124.125.254";
+                ctx.SubmitChanges();
+            }
+        }
+        private void Delete() {
+            //删除数据
+            var userloginlog = from u in ctx.UserLoginLogs where u.ID == 89 select u;
+            ctx.UserLoginLogs.DeleteAllOnSubmit(userloginlog);
+            ctx.SubmitChanges();
+        }
+    }
+}
---
+++ LinqLearn/DataBase/MyDB.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace LinqLearn.DataBase
+{
+    public class MyDataContext : DataContext
+    {
+        private static string connectionString { get {
+                System.Data.SqlClient.SqlConnectionStringBuilder builder = new System.Data.SqlClient.SqlConnectionStringBuilder();
+                builder["Data Source"] = "(local)";
+                builder["integrated Security"] = true;
+                builder["Initial Catalog"] = "testDB";
+                builder["UID"] = "sa";
+                builder["PWD"] = "123456";
+                return builder.ConnectionString;
+              } }
+
+        public Table<UserLoginLog> UserLoginLogs;
+        public MyDataContext()
+        :
+        base(connectionString)
+        {
+        }
+    }
+
+    [Table(Name = "user_online_log")]
+    public partial class UserLoginLog : INotifyPropertyChanged
+    {
+        private int _ID;
+        private string _User_ID;
+        private string _UserName;
+        private string _IP;
+        private string _TimeStamp;
+        public UserLoginLog() { }
+        [Column(IsPrimaryKey = true, Name = "id", IsDbGenerated = true, CanBeNull = false, DbType = "Int NOT NULL IDENTITY")]
+        public int ID {
+            get { return this._ID; }
+            set
+            {
+                if ((this._ID != value))
+                {
+                    this.OnPropertyChanged("id");
+                    this._ID = value;
+                    this.OnPropertyChanged("id");
+                }
+            }
+        }
+        [Column(Name = "user_id")]
+        public string User_ID {
+            get { return this._User_ID; }
+            set
+            {
+                if ((this._User_ID != value))
+                {
+                    this.OnPropertyChanged("user_id");
+                    this._User_ID = value;
+                    this.OnPropertyChanged("user_id");
+                }
+            }
+        }
+        [Column(Name = "user_name")]
+        public string UserName {
+            get { return this._UserName; }
+            set
+            {
+                if ((this._UserName != value))
+                {
+                    this.OnPropertyChanged("user_name");
+                    this._UserName = value;
+                    this.OnPropertyChanged("user_name");
+                }
+            }
+        }
+        [Column(Name = "ip")]
+        public string IP {
+            get { return this._IP; }
+            set
+            {
+                if ((this._IP != value))
+                {
+                    this.OnPropertyChanged("ip");
+                    this._IP = value;
+                    this.OnPropertyChanged("ip");
+                }
+            }
+        }
+        [Column(Name = "timestamp")]
+        public string TimeStamp {
+            get { return this._TimeStamp; }
+            set
+            {
+                if ((this._TimeStamp != value))
+                {
+                    this.OnPropertyChanged("timestamp");
+                    this._TimeStamp = value;
+                    this.OnPropertyChanged("timestamp");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            if ((this.PropertyChanged != null))
+            {
+                this.PropertyChanged(this,
+                new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+    }
+}
