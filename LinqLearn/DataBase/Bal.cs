4cb44659e76a2af172f50159ac04909375fab49a
anjoe <775083111@qq.com>
2019-03-13 10:54:06 +0800

    添加一个保护区

---
+++ LinqLearn/DataBase/Bal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinqLearn.DataBase
+{
+    public class Bal
+    {
+        //这个文件是添加到保护分支的
+    }
+}
