e05ad967498f780851ec1e2a38a6c949e06186f0
anjoe <775083111@qq.com>
2019-03-13 11:02:24 +0800

    测试一个不保留

---
+++ LinqLearn/DataBase/Dispass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinqLearn.DataBase
+{
+    public class Dispass
+    {
+        //保护区不允许保留提交了
+
+    }
+}
